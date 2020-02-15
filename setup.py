38143f762b39f677bbdc09ddf98e400948d0913d
Lodek <23224552+Lodek@users.noreply.github.com>
2020-02-15 16:05:09 -0300

    git ignore and setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='mycollections',
+    version='0.0.1',
+    author='Lodek',
+    description='Misc util classe',
+    packages=['mycollections'],
+    )
+
