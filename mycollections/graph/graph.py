18db6efd961a809d8e9ca7b93448ee845b9a357c
Lodek <23224552+Lodek@users.noreply.github.com>
2020-04-07 17:34:04 -0300

    directed graph interface

--- mycollections/graph/graph.py
+++ mycollections/graph/graph.py
@@ -4,21 +4,41 @@ Module to allow working with ordered and unordered graphs
 
 class Node:
     """
-    Models a node, stores a value and a list of nodes which are related to it
+    Models a node, store a node and the relationship between other nodes
+    as in a directed graphs (ie children and parensts)
     """
     value = 0
     _graph = None
 
     @property
-    def related_nodes(self):
-        return self._graph.get_related(self)
+    def parents(self):
+        """Return RelationSet object for self's parents"""
+        pass
+
+    @property
+    def children(self):
+        """Return RelationSet object for self's children"""
+        pass
 
 
-class RelatedNodes:
+class RelationSet:
     """
     Class to abstract relationship of a node.
-    Implements a subset of the set class
+    Implements a subset of the set class.
+    Represents either a children set or parent set relationship
     """
+    relations = set()
+    map = None
+
+    @classmethod
+    def build_parent_relation(cls, node, map):
+        """Return RelationSet for `node's` parent"""
+        pass
+
+    @classmethod
+    def build_children_relation(cls, node, map):
+        """Return RelationSet for `node's` children"""
+        pass
 
     def add(self):
         pass
@@ -29,17 +49,33 @@ class RelatedNodes:
     def __len__(self):
         pass
 
+    def __getitem__(self):
+        pass
+
    
-class Relations:
+class Map:
     """
     Store the relationships among all nodes for a given graph
     """
-    relationships = {}
+    relationships = {} # (Node -> ([Node], [Node]))
 
-    def get_related(self, node):
-        """Return all nodes related to `node`"""
+    def add_parent(self, node, parent):
+        """Add parent to node"""
         pass
 
+     def add_child(self, node, child):
+        """Add child to node"""
+        pass
+
+    def remove_parent(self, node, parent):
+        """Remove parent from node"""
+        pass
+
+    def remove_child(self, node, child):
+        """Remove child from parent"""
+        pass
+
+    
 
 class Graph:
     """
@@ -47,6 +83,13 @@ class Graph:
     """
     relations = None
     nodes = []
-    def node_factory(self, value, related_nodes=[]):
-        """Factory for creating a node with a given value and list of siblings"""
+    node_factory = lambda node, related_nodes : None
+
+    def remove_node(self, node):
+        """Remove node from graph and erase its relationships"""
         pass
+
+    def union(self, graph):
+        """Return Union of two graph objects"""
+        pass
+
