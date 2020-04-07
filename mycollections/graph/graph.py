088e3b8ab0dad9f7714ebad9f858ed3233787506
Lodek <23224552+Lodek@users.noreply.github.com>
2020-04-07 15:59:17 -0300

    related nodes class

--- mycollections/graph/graph.py
+++ mycollections/graph/graph.py
@@ -7,19 +7,46 @@ class Node:
     Models a node, stores a value and a list of nodes which are related to it
     """
     value = 0
+    _graph = None
+
+    @property
+    def related_nodes(self):
+        return self._graph.get_related(self)
 
 
+class RelatedNodes:
+    """
+    Class to abstract relationship of a node.
+    Implements a subset of the set class
+    """
+
+    def add(self):
+        pass
+        
+    def remove(self):
+        pass
+    
+    def __len__(self):
+        pass
+
+   
 class Relations:
     """
     Store the relationships among all nodes for a given graph
     """
     relationships = {}
 
+    def get_related(self, node):
+        """Return all nodes related to `node`"""
+        pass
+
 
 class Graph:
     """
     Provides interface to work with graph
     """
-
+    relations = None
+    nodes = []
     def node_factory(self, value, related_nodes=[]):
+        """Factory for creating a node with a given value and list of siblings"""
         pass
